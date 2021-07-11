663da8f4be3739f48a07006088a1b30ce817d633
Blankeos <carloantonioct@gmail.com>
2021-07-12 07:11:57 +0800

    Fixed footer link to catt.me

--- blog-app/components/Footer.tsx
+++ blog-app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-black text-white flex justify-center items-center py-3 flex-col space-y-2 text-sm">
       <p>2021 © Carlo Taleon. All Rights Reserved.</p>
-      <Link href="catt.me">Carlo Taleon's Blog Website</Link>
+      <Link href="https://catt.me">Carlo Taleon's Blog Website</Link>
     </footer>
   );
 };
