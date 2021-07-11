50be96eedb0d4b3aca07f53136d744b62c5e5259
Blankeos <carloantonioct@gmail.com>
2021-07-12 07:12:16 +0800

    Changed some props and styles for article link

--- blog-app/components/Home/ArticleLink.tsx
+++ blog-app/components/Home/ArticleLink.tsx
@@ -6,15 +6,31 @@ interface Props {
   image?: string;
   title: string;
   shortDesc?: string;
+  dateString: string;
 }
 
-const ArticleLink: React.FC<Props> = ({ link, image, title, shortDesc }) => {
+const ArticleLink: React.FC<Props> = ({
+  link,
+  image,
+  title,
+  dateString,
+  shortDesc,
+}) => {
   return (
     <Link href={link ? link : "/"}>
       <a>
-        <div className="border p-4">
-          <h3 className="font-medium">{title}</h3>
-          <p className="text-sm">{shortDesc}</p>
+        <div className="border p-4 group">
+          <div className="flex text-sm text-gray-500">
+            Posted in{" "}
+            {dateString ? new Date(dateString).toLocaleDateString() : "Unknown"}
+          </div>
+          <h3 className="font-medium text-xl my-2 group-hover:text-yellow-500 transition">
+            {title}
+          </h3>
+          <p className="">{shortDesc}</p>
+          <p className="mt-4 text-sm text-gray-500 group-hover:translate-x-2 transform transition">
+            Read more &rarr;
+          </p>
         </div>
       </a>
     </Link>
